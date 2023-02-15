870c2cd0c02af51af137e30bb76da456efaa8257
teuddycr <88355800+teuddycr@users.noreply.github.com>
2023-02-15 15:14:52 -0400

    creted some endpoints to get and list customers

--- src/main/java/com/teuddycr/movies/infra/inputadapter/CustomerInputAdapter.java
+++ src/main/java/com/teuddycr/movies/infra/inputadapter/CustomerInputAdapter.java
@@ -1,6 +1,9 @@
 package com.teuddycr.movies.infra.inputadapter;
 
 import org.springframework.http.MediaType;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,16 @@ public class CustomerInputAdapter {
     @PostMapping(value = "create", produces = MediaType.APPLICATION_JSON_VALUE)
     public Customer create(@RequestParam String name, @RequestParam String country) {
         return customerInputPort.createCustomer(name, country);
+    }
+
+    @PostMapping(value = "get", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Customer get(@RequestParam String customerId) {
+        return customerInputPort.getById(customerId);
+    }
+
+    @PostMapping(value = "getall", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Customer> getAll() {
+        return customerInputPort.getAll();
     }
 
 }
